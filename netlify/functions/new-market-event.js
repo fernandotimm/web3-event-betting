ff784384efa64a6bc4ccf73a4c9eb3c42834dffd
Guilherme Mussi <gmussi@gmail.com>
2022-05-24 15:55:08 +0200

    deploy market watcher function

--- netlify/functions/new-market-event.js
+++ netlify/functions/new-market-event.js
@@ -1,9 +1,37 @@
-const {MongoClient} = require('mongodb');
+const axios = require("axios");
 
 exports.handler = async function(event, context) {
-    console.log("body", event.body);
-    console.log("parsed", JSON.parse(event.body));
+    let body = JSON.parse(event.body);
 
+    body.events.forEach((event) => {
+        event.matchReasons.forEach((obj) => {
+            let { marketId, oracle, closingTime, numOutcomes } = obj.params;
+            
+            const response = await axios.post(
+                "https://data.mongodb-api.com/app/data-dpoyr/endpoint/data/beta/action/insertOne",
+                {
+                    "dataSource": "Cluster0",
+                    "database": "distamarkets",
+                    "collection": "markets",
+                    "document": {
+                        marketId,
+                        oracle,
+                        closingTime,
+                        numOutcomes
+                    }
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Request-Headers": "*",
+                        "api-key": process.env.DATA_API_KEY
+                    }
+                }
+            );
+
+            console.log(response.status, response.data);
+        });
+    });
     return {
         statusCode: 200,
         body: "OK"
